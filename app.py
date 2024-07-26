23ce78809202582032234464c050c81d3f6dbabc
christopher2353 <christopherdzfrance@gmail.com>
2024-07-26 11:29:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title("Welcome to My First Streamlit App")
 name = st.text_input("Enter your name")
 
 if name:
-  st.write(f"Hello, {name}! Welcome to the app")
+  st.write(f"Hello, {name}! Welcome to my app")
 
 
 
